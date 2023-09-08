6c6cb0a927a80f2384170a25ff55aa41774f0056
mc2259 <mc2259@cornell.edu>
2023-09-08 08:44:16 -0700

    Removed monitor

--- train.py
+++ train.py
@@ -139,7 +139,6 @@ def main():
     checkpoint_callback = ModelCheckpoint(
         dirpath=args.ckpt_dir,
         save_top_k=2,
-        monitor='val/loss',
         mode='min',
         save_weights_only=True,
         filename='{epoch}', # this cannot contain slashes 
