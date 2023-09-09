e61a620499c9e2109a2434c6752ae0053ea3eade
mc2259 <mc2259@cornell.edu>
2023-09-09 14:46:37 -0700

    Added comma

--- train.py
+++ train.py
@@ -139,7 +139,7 @@ def main():
     checkpoint_callback = ModelCheckpoint(
         dirpath=args.ckpt_dir,
         save_top_k=2,
-        monitor='valid_loss',
+        monitor='val/loss',
         mode='min',
         save_weights_only=True,
         filename='{epoch}', # this cannot contain slashes 
@@ -172,7 +172,7 @@ def main():
         min_epochs=args.num_train_epochs,
         max_epochs=args.num_train_epochs, 
         gpus=args.gpus, 
-        callbacks = [checkpoint_callback], 
+        callbacks = [checkpoint_callback, logger, lr_logger], 
         accumulate_grad_batches=args.accumulate_grad_batches,
         gradient_clip_val=args.gradient_clip_val, 
         num_sanity_val_steps=0, 
