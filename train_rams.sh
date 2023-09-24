e2fbafc31e893db77fb4f84309555119c14a6d39
mc2259 <mc2259@cornell.edu>
2023-09-24 13:31:10 -0700

    Added consolidated data

--- train_rams.sh
+++ train_rams.sh
@@ -4,9 +4,9 @@ set -x
 
 python train.py --model=gen --ckpt_name='gen-RAMS' \
     --dataset=RAMS \
-    --train_file=data/RAMS_1.0/data/train.jsonlines \
-    --val_file=data/RAMS_1.0/data/dev.jsonlines \
-    --test_file=data/RAMS_1.0/data/test.jsonlines \
+    --train_file=data/RAMS_1.0/data/train.jsonl \
+    --val_file=data/RAMS_1.0/data/dev.jsonl \
+    --test_file=data/RAMS_1.0/data/test.jsonl \
     --train_batch_size=2 \
     --eval_batch_size=4 \
     --learning_rate=3e-5 \
