d82d13084c88d94a827f18258a5345e862e04b57
mc2259 <mc2259@cornell.edu>
2023-09-25 19:17:14 -0700

    Finallll

--- test_rams.sh
+++ test_rams.sh
@@ -5,7 +5,7 @@ CKPT_NAME=gen-RAMS
 MODEL=gen
 
 python train.py --model=$MODEL --ckpt_name=$CKPT_NAME-pred \
-    --load_ckpt=checkpoints/$CKPT_NAME/epoch=2-v0.ckpt \
+    --load_ckpt=checkpoints/$CKPT_NAME/epoch=2-v1.ckpt \
     --dataset=RAMS \
     --eval_only \
     --train_file=data/RAMS_1.0/data/train.jsonlines \
@@ -19,21 +19,21 @@ python train.py --model=$MODEL --ckpt_name=$CKPT_NAME-pred \
 
 
 #span eval 
-python genie/convert_gen_to_output.py --gen-file=checkpoints/$CKPT_NAME-pred/predictions.jsonl \
+python src/genie/convert_gen_to_output.py --gen-file=checkpoints/$CKPT_NAME-pred/predictions.jsonl \
 --output-file=checkpoints/$CKPT_NAME-pred/span_output.jsonl 
 
 python data/RAMS_1.0/scorer/scorer.py -g=data/RAMS_1.0/data/test.jsonlines -p=checkpoints/$CKPT_NAME-pred/span_output.jsonl \
 --reuse_gold_format --do_all > checkpoints/$CKPT_NAME-pred/span_metrics.txt 
 
 # head eval
-python genie/convert_gen_to_output.py --gen-file=checkpoints/$CKPT_NAME-pred/predictions.jsonl \
+python src/genie/convert_gen_to_output.py --gen-file=checkpoints/$CKPT_NAME-pred/predictions.jsonl \
 --output-file=checkpoints/$CKPT_NAME-pred/output.jsonl --head-only
 
 python data/RAMS_1.0/scorer/scorer.py -g=data/RAMS_1.0/data/test_head.jsonlines -p=checkpoints/$CKPT_NAME-pred/output.jsonl \
 --reuse_gold_format --do_all > checkpoints/$CKPT_NAME-pred/head_metrics.txt 
 
 # head + coref eval 
-python genie/convert_gen_to_output.py --gen-file=checkpoints/$CKPT_NAME-pred/predictions.jsonl \
+python src/genie/convert_gen_to_output.py --gen-file=checkpoints/$CKPT_NAME-pred/predictions.jsonl \
 --test-file=data/RAMS_1.0/data/test_head_coref.jsonlines \
 --output-file=checkpoints/$CKPT_NAME-pred/coref_output.jsonl --head-only --coref
 
