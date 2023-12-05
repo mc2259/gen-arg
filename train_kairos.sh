da43a394e3bdd3ebd8ba66cac2506059012ea301
mc2259 <mc2259@cornell.edu>
2023-12-05 11:20:44 -0800

    added

--- train_kairos.sh
+++ train_kairos.sh
@@ -8,13 +8,13 @@ rm -rf checkpoints/${CKPT_NAME}
 # does not use informative mentions 
 python3 train.py --model=constrained-gen --ckpt_name=${CKPT_NAME} \
     --dataset=KAIROS \
-    --train_file=data/wikievents/no-triggers_train.jsonl \
-    --val_file=data/wikievents/no-triggers-dev.jsonl \
-    --test_file=data/wikievents/no-triggers-test.jsonl \
+    --train_file=data/wikievents/train.jsonl \
+    --val_file=data/wikievents/dev.jsonl \
+    --test_file=data/wikievents/test.jsonl \
     --train_batch_size=2 \
     --eval_batch_size=4 \
     --learning_rate=3e-5 \
     --accumulate_grad_batches=8 \
     --num_train_epochs=2 \
-    --mark_trigger \
+     --mark_trigger=false \
     --coref_dir=data/wikievents/coref
