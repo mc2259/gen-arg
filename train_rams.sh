4010898fc3d79786016391da33a5d40a0c86eb15
mc2259 <mc2259@cornell.edu>
2024-03-24 22:27:23 -0700

    Changed data

--- train_rams.sh
+++ train_rams.sh
@@ -4,9 +4,9 @@ set -x
 
 python train.py --model=gen --ckpt_name='gen-RAMS' \
     --dataset=RAMS \
-    --train_file=data/RAMS_1.0/data/wiki-llm-train.jsonl \
-    --val_file=data/RAMS_1.0/data/wiki-llm-dev.jsonl \
-    --test_file=data/RAMS_1.0/data/wiki-llm-test.jsonl \
+    --train_file=data/RAMS_1.0/data/wiki-human-train.jsonl \
+    --val_file=data/RAMS_1.0/data/wiki-human-dev.jsonl \
+    --test_file=data/RAMS_1.0/data/wiki-human-test.jsonl \
     --train_batch_size=2 \
     --eval_batch_size=4 \
     --learning_rate=3e-5 \
