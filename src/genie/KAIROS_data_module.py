215f84fb73a19a47f9ef51ec71235fe756ba6721
mc2259 <mc2259@cornell.edu>
2024-05-11 22:54:35 -0700

    Added

--- src/genie/KAIROS_data_module.py
+++ src/genie/KAIROS_data_module.py
@@ -164,13 +164,52 @@ class KAIROSDataModule(pl.LightningDataModule):
         print('input template:{}'.format(input_template))
         print('output template:{}'.format(output_template))
         return tokenized_input_template, tokenized_template, context
+    def load_ontology(self):
+        # read ontology 
+        # ontology_dict = load_ontology('KAIROS')
+        # keys_to_modify = []
+        # for key in ontology_dict.keys():
+        #     new_key = key.split(".")[0] + "." + key.split(".")[1]
+        #     if new_key != key:
+        #         keys_to_modify.append((key, new_key))
 
+        # for key, new_key in keys_to_modify:
+        #     ontology_dict[new_key] = ontology_dict.pop(key)
+
+        # for key in ontology_dict.keys():
+        #     args = ontology_dict[key]['roles']
+        #     for i, arg in enumerate(args):
+        #         if arg != '':
+        #             ontology_dict[key]['arg{}'.format(i+1)] = arg 
+        #             ontology_dict[key][arg] = 'arg{}'.format(i+1)
+        ontology_dict ={} 
+        with open('aida_ontology_cleaned.csv','r') as f:
+            for lidx, line in enumerate(f):
+                if lidx == 0:# header 
+                    continue 
+                fields = line.strip().split(',') 
+                if len(fields) < 2:
+                    break 
+                evt_type = fields[0]
+                args = fields[2:]
+                
+                ontology_dict[evt_type] = {
+                        'template': fields[1]
+                    }
+                
+                for i, arg in enumerate(args):
+                    if arg !='':
+                        ontology_dict[evt_type]['arg{}'.format(i+1)] = arg 
+                        ontology_dict[evt_type][arg] = 'arg{}'.format(i+1)
+        first_key, first_value = next(iter(ontology_dict.items()))
+        # print("Ontology", first_key, first_value)
+        return ontology_dict 
     
     def prepare_data(self):
         data_dir = 'preprocessed_{}'.format(self.hparams.dataset)
         if not os.path.exists(data_dir):
             os.makedirs(data_dir)
-            ontology_dict = load_ontology(self.hparams.dataset)
+            ontology_dict = self.load_ontology()
             keys_to_modify = []
             for key in ontology_dict.keys():
                 new_key = key.split(".")[0] + "." + key.split(".")[1]
